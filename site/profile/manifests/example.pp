c9a61d6a6e7cfbd3ad61ea9f7324832e8492cd13
Robert Kilgore <rkilgore@columnit.com>
2017-01-12 11:05:09 -0500

    adding depends

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -1,13 +1,2 @@
 class profile::example {
-   registry::value { 'Legal notice caption':
-     key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
-     value => 'legalnoticecaption',
-     data  => 'Legal Notice',
-     }
-
-   registry::value { 'Legal notice text':
-     key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
-     value => 'legalnoticetext',
-     data  => 'Login constitutes acceptance of the End User Agreement',
-     }
 }
