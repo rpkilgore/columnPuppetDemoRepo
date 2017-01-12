ad0d8bfb78c13c2c59d258b82dbab76c43fb022f
Robert Kilgore <rkilgore@columnit.com>
2017-01-12 10:56:27 -0500

    fixing..

--- site/critical_policy/manifests/init.pp
+++ site/critical_policy/manifests/init.pp
@@ -1,12 +1,12 @@
 class critical_policy {
 
-   registry_value { 'Legal notice caption':
+   registry::value { 'Legal notice caption':
      key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
      value => 'legalnoticecaption',
      data  => 'Legal Notice',
      }
 
-   registry_value { 'Legal notice text':
+   registry::value { 'Legal notice text':
      key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
      value => 'legalnoticetext',
      data  => 'Login constitutes acceptance of the End User Agreement',
