05cc431a680e25bc7e0e2ef2f37e7a688f59d87e
Robert Kilgore <rkilgore@columnit.com>
2017-01-11 22:32:54 -0500

    legal notice caption

---
+++ site/critical_policy/manifests/init.pp
@@ -0,0 +1,14 @@
+ class critical_policy {
+
+   registry::value { 'Legal notice caption':
+     key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
+     value => 'legalnoticecaption',
+     data  => 'Legal Notice',
+     }
+
+   registry::value { 'Legal notice text':
+     key   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
+     value => 'legalnoticetext',
+     data  => 'Login constitutes acceptance of the End User Agreement',
+     }
+   }
